1c629aae75cb1cce373eb4fb7646c272e6a1dfda
crtier <160504674+crtier@users.noreply.github.com>
2025-04-28 18:48:07 +0400

    change default model
    mistral is now paid, so i replaced it with bart

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ import requests
 
 app = Flask(__name__)
 
-# YOUR API-key Hugging Face
 HUGGINGFACE_API_KEY = "replace with your API-key"
-MODEL_NAME = "mistralai/Mistral-7B-Instruct-v0.2"
+MODEL_NAME = "facebook/bart-large-cnn"
 API_URL = f"https://api-inference.huggingface.co/models/{MODEL_NAME}"
 
 headers = {"Authorization": f"Bearer {HUGGINGFACE_API_KEY}"}
@@ -18,27 +17,22 @@ def home():
 def summarize():
     try:
         data = request.json
-        text = data.get("text", "").strip()
+        text = data.get("text", "")
 
         if not text:
-            return jsonify({"error": "Введите текст для обработки"}), 400
-
-        payload = {
-            "inputs": text,
-            "parameters": {"max_new_tokens": 200, "temperature": 0.7}
-        }
-
-        response = requests.post(API_URL, headers=headers, json=payload)
+            return jsonify({"error": "No text provided"}), 400
+        
+        response = requests.post(API_URL, headers=headers, json={"inputs": text})
 
         if response.status_code == 200:
             result = response.json()
-            summary = result[0]["generated_text"]
+            summary = result[0]["summary_text"]
             return jsonify({"summary": summary})
         else:
-            return jsonify({"error": "Ошибка запроса", "details": response.json()}), response.status_code
+            return jsonify({"error": "Failed to fetch summary", "details": response.json()}), response.status_code
 
     except Exception as e:
-        return jsonify({"error": "Произошла ошибка", "details": str(e)}), 500
+        return jsonify({"error": "An error occurred", "details": str(e)}), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
